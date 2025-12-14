8c23ba4bf5c2b8f18c144246220eead12dff7dca
shri29s <shricharan.r24@iiits.in>
2025-12-14 23:09:33 +0530

    data ingestion config

---
+++ networksecurity/constants/__init__.py
@@ -0,0 +1,16 @@
+# Common constants
+TARGET_COLUMN="Result"
+PIPELINE_NAME="NetworkSecurity"
+ARTIFACT_DIR="Artifacts"
+FILE_NAME="phisingData.csv"
+
+TRAIN_FILE_NAME="train.csv"
+TEST_FILE_NAME="test.csv"
+
+# Data Ingestion
+DATA_INGESTION_COLLECTION_NAME="phisingData"
+DATA_INGESTION_DATABASE_NAME="phisingData"
+DATA_INGESTION_DIR_NAME="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR="feature_store"
+DATA_INGESTION_INGESTED_DIR="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO=0.2
---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,43 @@
+from networksecurity import constants
+from datetime import datetime
+import os
+
+class TrainingPipelineConfig:
+    def __init__(self, timestamp = datetime.now()):
+        timestamp = timestamp.strftime("%d_%m_%y_%H_%M_%S")
+        
+        self.pipeline_name = constants.PIPELINE_NAME
+        self.artifact_name = constants.ARTIFACT_DIR
+        self.timestamp = timestamp
+
+        self.artifact_dir = os.path.join(self.artifact_name, self.timestamp)
+
+
+class DataIngestionConfig:
+    def __init__(self, training_config: TrainingPipelineConfig):
+        self.data_ingestion_dir = os.path.join(
+            training_config.artifact_dir,
+            constants.DATA_INGESTION_DIR_NAME
+        )
+
+        self.feature_store_file_path = os.path.join(
+            self.data_ingestion_dir,
+            constants.DATA_INGESTION_FEATURE_STORE_DIR,
+            constants.FILE_NAME
+        )
+
+        self.training_file_path = os.path.join(
+            self.data_ingestion_dir,
+            constants.DATA_INGESTION_INGESTED_DIR,
+            constants.TRAIN_FILE_NAME
+        )
+
+        self.testing_file_path = os.path.join(
+            self.data_ingestion_dir,
+            constants.DATA_INGESTION_INGESTED_DIR,
+            constants.TEST_FILE_NAME
+        )
+
+        self.train_test_ratio = constants.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+        self.collection_name = constants.DATA_INGESTION_COLLECTION_NAME
+        self.database_name = constants.DATA_INGESTION_DATABASE_NAME
